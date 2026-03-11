88db9f5b182e64c2969038566d6c510a874aab38
00Bhuwan <9843lalitpur@gmail.com>
2026-03-11 23:55:44 +0545

    new changes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from db import db
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+@app.route("/home")
+def home():
+    ...
